e5f91081e73bc20bd0a6d214fba5b1b2e494dcca
Kevin <kevinzhang237@email.arizona.edu>
2021-08-04 23:03:50 -0700

    method stubs

--- server.js
+++ server.js
@@ -13,21 +13,26 @@ const config = {
 
 //// ----- requires/imports -----
 // express server
-const server = require('express');
+const express = require('express');
 const sessionMiddleware = require('express-sessions');
 const bodyParserMiddleware = require('body-parser');
 // mongoose
-const mongo = require('mongoose');
+const mongoose = require('mongoose');
+
+//// ----- instantiate server and db -----
+const server = express();
+server.use(sessionMiddleware({secret: 'ourSecret'}));
+server.use(bodyParserMiddleware());
 
 //// ----- object defines, constructors -----
 // client-facing objects: these are serialized directly to the client.
-function GameState(IsPlaying, IsFinished, GameBoard, PlayerScore, ComputerScore, Winner){
-    this.isPlaying = IsPlaying;
+function GameState(IsFinished, GameBoard, PlayerScore, ComputerScore, Winner, Player){
     this.isFinished = IsFinished;
     this.board = GameBoard;
     this.playerScore = PlayerScore;
     this.computerScore = ComputerScore;
     this.winner = Winner;
+    this.palyer = Player;
 }
 
 function SessionState(Username){
@@ -40,7 +45,18 @@ function ScoreboardEntry(username, score){
     this.score = score;
 }
 
+// responses from get/post functions
+function Success(message){
+    this.errored = false;
+    this.message = message;
+}
+function Fail(message){
+    this.errored = true;
+    this.message = message;
+}
+
 // shared objects
+// board coordinates are x-y from lower left corner.
 function Board(){
     this.array = [];
     this.array.length = 8;
@@ -53,20 +69,138 @@ function Board(){
 function Piece(){
     this.owner = null;
     this.king = false;
+    this.x = null;
+    this.y = null;
 }
 
 // serverside only
-function User(username, password, uid){
+function User(username, password){
     this.username = username;
     this.password = password;
-    this.uid = uid;
+    this.totalScore = 0;
 }
 
-//// ----- helpers and processing: checkers -----
+//// ----- database "schema" ----- 
+
+// the two things we need to store right now
+
+// user information: username, password, uid, score, game uid
+// uid is the "primary key" of a user.
+
+// game state information: finished, board, current score for the user and computer, who won (if finished), and game uid
+// game uid identifies a game uniquely
+
+// in mongoose, _id is uid.
+
+const UserSchema = new mongoose.Schema({username: mongoose.Schema.Types.String, password: mongoose.Schema.Types.String, activegame: mongoose.Schema.Types.ObjectId, totalScore: mongoose.Schema.Types.Number});
+const GameStateSchema = new mongoose.Schema({board: mongoose.Schema.Types.Mixed, finished: mongoose.Schema.Types.Boolean, playerScore: mongoose.Schema.Types.Number, computerScore: mongoose.Schema.Types.Number, player: mongoose.Schema.Types.ObjectId});
+
+const UserModel = mongoose.model('user', UserSchema);
+const GameStateModel = mongoose.model('gamestate', GameStateSchema);
 
 //// ----- helpers and processing: users and sessions -----
 
+// for users, a username/password is sufficient
+// but we also need to store the game id of any individual user
+// furthermore, guest players must be usable, without recording to our database.
+function createUser(username, password){
+
+}
+
+function getUserByName(username){
+    
+}
+
+// returns User object on success, or null.
+function attemptLogin(username, password){
+    
+}
+
+// gets the game state for a specific user
+function getGameStateByName(username){
+
+}
+
+// increments a user's score by this much
+function incrementScoreByName(username, score){
+
+}
+
+// gets a list of top scoring users
+function getHighScoreList(amount){
+    
+}
+
+//// ----- helpers and processing: checkers -----
+// perform a single AI 'tick' on a game state
+function aiMove(gamestate){
+
+}
+
+// instantiate a new game
+// returns the gamestate
+function newGame(){
+
+}
+
+// checks for victory. returns null, true for player, or false for computer.
+function checkVictory(){
+    
+}
+
+// lists valid 3-tuples of coordinates a piece can move to, as [x, y, captures]
+function validMoves(piece){
+
+}
+
+// moves a piece capturing all pieces along the way.
+function move(piece, x, y){
+
+}
+
+// captures a piece
+function capture(piece){
+    
+}
+
+// kings a piece
+function king(piece){
+    
+}
+
 //// ----- express setup and bindings -----
 
+// GET: session state
+server.get("/site_api/session", (req, res) => {
+    
+})
+
+// GET: game state
+server.get("/site_api/game", (req, res) => {
+    
+})
+
+// POST: attempt login
+server.post("/site_api/login", (req, res) =>{
+
+})
+
+// GET: highscore rankings
+server.get("/site_api/scoreboard", (req, res) => {
+    const max = 30;     // default maximum number of users to get
+
+})
+
+// POST: account creation
+server.post("/site_api/create_account", (req, res) =>{
+    
+})
+
+// POST: game move
+// input: x1, y1, x2, y2
+// if successful, the client should request game state again.
+server.post("/site_api/move_piece", (req, res) =>{
+    
+})
 
 
